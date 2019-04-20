5712f1f3bdc31537daf9f9b59e0789e3bbba07a4
jjysh1 <jjyshj@kcpmed.com>
2019-04-20 16:20:04 +0900

    XML 수정 레이아웃 잘못된 부분 수정

--- app/src/main/java/com/example/jicbanglistview/MainActivity.java
+++ app/src/main/java/com/example/jicbanglistview/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.ColorSpace;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.jicbanglistview.adapter.RoomAdapter;
 import com.example.jicbanglistview.data.Room;
 import com.example.jicbanglistview.databinding.ActivityMainBinding;
 
@@ -12,6 +13,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RoomAdapter mRoomAdapter;
+
 
     List<Room> roomList = new ArrayList<>();
 
@@ -24,10 +28,25 @@ public class MainActivity extends AppCompatActivity {
         act = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
         act.cleanRoomCountTxt.setText("32개");
+        fillRooms();
 
+        mRoomAdapter = new RoomAdapter(MainActivity.this, roomList);
 
+        act.roomList.setAdapter(mRoomAdapter);
     }
     void fillRooms(){
+        roomList.add(new Room(500, 30, "서구 둔산동",7,"마트인근"));
+        roomList.add(new Room(1000, 50, "서구 일동",7,"채광굿"));
+        roomList.add(new Room(2000, 100, "북구 광동",5,"학교인근"));
+        roomList.add(new Room(300, 10, "동구 제일동",1,"경찰서 뒷편"));
+        roomList.add(new Room(1500, 25, "북구 오치동",3,"화장실 3개"));
+        roomList.add(new Room(2500, 30, "서구 상가동",2,"마트인근"));
+        roomList.add(new Room(500, 30, "서구 둔산동",7,"마트인근"));
+        roomList.add(new Room(1000, 50, "서구 일동",7,"채광굿"));
+        roomList.add(new Room(2000, 100, "북구 광동",5,"학교인근"));
+        roomList.add(new Room(300, 10, "동구 제일동",1,"경찰서 뒷편"));
+        roomList.add(new Room(1500, 25, "북구 오치동",3,"화장실 3개"));
+        roomList.add(new Room(2500, 30, "서구 상가동",2,"마트인근"));
         roomList.add(new Room(500, 30, "서구 둔산동",7,"마트인근"));
         roomList.add(new Room(1000, 50, "서구 일동",7,"채광굿"));
         roomList.add(new Room(2000, 100, "북구 광동",5,"학교인근"));
